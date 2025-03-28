c78a56268f396c8c8bea8dea17f2f0d1d78faacf
daehan-lim <penjan.en@gmail.com>
2025-03-28 16:50:10 +0900

    Finish seat_bottom UI

--- lib/ui/seat/widgets/seat_bottom.dart
+++ lib/ui/seat/widgets/seat_bottom.dart
@@ -14,17 +14,22 @@ class SeatBottom extends StatelessWidget {
       ),
       child: Column(
         children: [
+          SizedBox(height: 20),
           Text(
             '선택된 좌석이 없습니다',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
+          SizedBox(height: 20),
           SizedBox(
             width: 200,
             height: 56,
             child: ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
-              child: Text('Book now'),
+              child: Text(
+                'Book now',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             ),
           ),
         ],
