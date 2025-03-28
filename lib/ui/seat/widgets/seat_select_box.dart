c7448ee21c59a37f00d0f5200b365eb7eb27a60d
daehan-lim <penjan.en@gmail.com>
2025-03-28 12:30:30 +0900

    Finish Seat Select Box static stateless

--- lib/ui/seat/widgets/seat_select_box.dart
+++ lib/ui/seat/widgets/seat_select_box.dart
@@ -5,7 +5,7 @@ class SeatSelectBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return const Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -13,29 +13,63 @@ class SeatSelectBox extends StatelessWidget {
             'Screen',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Text(
-                      '1',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                for (var i = 0; i < 10; i++) seat(),
-              ],
-            ),
+          SizedBox(height: 10),
+          SeatRow(1),
+          SizedBox(height: 10),
+          SeatRow(2),
+          SizedBox(height: 10),
+          SeatRow(3),
+          SizedBox(height: 10),
+          SeatRow(4),
+          SizedBox(height: 10),
+          SeatRow(5),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SeatInfo('Available', Colors.grey),
+              SizedBox(width: 4),
+              SeatInfo('Selected', Colors.amber),
+            ],
           ),
         ],
       ),
     );
   }
+}
 
-  Widget seat() {
+class SeatRow extends StatelessWidget {
+  final int rowNum;
+
+  const SeatRow(this.rowNum, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                '$rowNum',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          for (var i = 0; i < 10; i++) Seat(),
+        ],
+      ),
+    );
+  }
+}
+
+class Seat extends StatelessWidget {
+  const Seat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Expanded(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 2),
@@ -49,6 +83,31 @@ class SeatSelectBox extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class SeatInfo extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  const SeatInfo(this.text, this.color, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(text, style: TextStyle(fontSize: 14)),
+        SizedBox(width: 4),
+        Container(
+          width: 20,
+          height: 20,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ),
+      ],
     );
   }
 }
