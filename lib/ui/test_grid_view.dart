43784c5fe185ec96c75b1b7a5f8dc7a5c8fe37ac
daehan-lim <penjan.eng@gmail.com>
2025-04-13 15:31:11 +0900

    Place 3 grid views in a SingleChildScrollView with a Column with a single vertical scroll

--- lib/ui/test_grid_view.dart
+++ lib/ui/test_grid_view.dart
@@ -9,72 +9,73 @@ class TestGridView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: GridView.builder(
-              padding: EdgeInsets.all(10),
-              itemCount: 10,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: 1,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              GridView.builder(
+                padding: EdgeInsets.all(10),
+                shrinkWrap: true, // ✅ make it fit its content
+                physics: NeverScrollableScrollPhysics(), // ✅ disable internal scrolling
+                itemCount: 10,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                ),
+                itemBuilder: (context, index) {
+                  return AppCachedImage(
+                    imageUrl: 'https://picsum.photos/seed/$index/300/300',
+                  );
+                },
               ),
-              itemBuilder: (context, index) {
-                return AppCachedImage(
-                  imageUrl: 'https://picsum.photos/seed/$index/300/300',
-                );
-              },
-            ),
-          ),
 
-          SizedBox(height: 20),
+              SizedBox(height: 20),
 
-          Flexible(
-            child: GridView.builder(
-              padding: EdgeInsets.all(20),
-              itemCount: 10,
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 170,
-                childAspectRatio: 1,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
+              GridView.builder(
+                padding: EdgeInsets.all(20),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 10,
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 170,
+                  childAspectRatio: 1,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) {
+                  return AppCachedImage(
+                    imageUrl: 'https://picsum.photos/seed/$index/300/300',
+                    fit: BoxFit.cover,
+                  );
+                },
               ),
-              itemBuilder: (context, index) {
-                return AppCachedImage(
-                  imageUrl: 'https://picsum.photos/seed/$index/300/300',
-                  fit: BoxFit.cover,
-                );
-              },
-            ),
-          ),
 
-          SizedBox(height: 20),
+              SizedBox(height: 20),
 
-          Flexible(
-            child: GridView(
-              padding: EdgeInsets.all(10),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: 1,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
+              GridView(
+                padding: EdgeInsets.all(10),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                ),
+                children: [
+                  AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
+                  AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
+                  AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
+                  AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
+                  AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
+                  AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
+                  AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
+                ],
               ),
-              children: [
-                AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
-                AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
-                AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
-                AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
-                AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
-                AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
-                AppCachedImage(imageUrl: 'https://picsum.photos/300/300'),
-              ],
-            ),
+            ],
           ),
-        ],
-      ),
+        ),
     );
   }
 }
